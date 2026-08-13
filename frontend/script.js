c066b3bf25fc8ed127a2c981093d50f8707a50a2
ramagirinagateja <ramagirinagateja@gmail.com>
2026-08-13 17:02:25 +0530

    ntg

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,14 @@
-alert("hey this is my first js file");
+//alert("hey this is my first js file");
+
+//document.querySelector("img").src=jane.png;
+let h2=document.querySelector("h2");
+let btn=document.getElementById("toggle");
+let img=document.querySelector("img");
+let p=document.querySelector("p");
+btn.addEventListener("click",function(){
+    h2.innerText="jane";
+    img.src="jane.png";
+    p.innerText="female";
+
+});
+
