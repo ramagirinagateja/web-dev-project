a37ec96b843e3dbb347edc670f7193d69f847cc5
ramagirinagateja <ramagirinagateja@gmail.com>
2026-08-14 10:34:24 +0530

    frontend

--- frontend/script.js
+++ frontend/script.js
@@ -5,10 +5,30 @@ let h2=document.querySelector("h2");
 let btn=document.getElementById("toggle");
 let img=document.querySelector("img");
 let p=document.querySelector("p");
+let count=0;
 btn.addEventListener("click",function(){
+    if(count==0){
     h2.innerText="jane";
     img.src="jane.png";
     p.innerText="female";
+    }
+    else if(count==1){
+        //3rd person data
+        h2.innerText="teja";
+        img.src="john.png";
+        p.innerText="female";
+    }
+    else if(count==2){
+        //4th person data
+    }
+    count++;
+    if(count==3){
+        count = 0;
+    }
 
 });
-
+let rdm=document.getElementById("random");
+rdm.addEventListener("click",function(){
+    //url:https://randomuser.me/api
+    fetch("https://randomuser.me/api ");
+});
